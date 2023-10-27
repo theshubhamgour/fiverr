491a1f259b2784c490dbac606a7848eaa3682a8d
Shubham Gour <iamtheshubhamgour@gmail.com>
2023-10-27 14:00:32 +0530

    Added Fiverr Web Build

---
+++ src/main/java/com/fiverr/App.java
@@ -0,0 +1,13 @@
+package com.fiverr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
